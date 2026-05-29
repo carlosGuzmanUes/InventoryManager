70bc9d416545b9f7dc17e969a2876d180fbcbf8b
GF12016 Carlos Guzman <gf12016@ues.edu.sv>
2026-05-29 09:33:57 -0600

    se agrega el authController y SecurityConfig

--- src/main/java/com/nothing/none/config/SecurityConfig.java
+++ src/main/java/com/nothing/none/config/SecurityConfig.java
@@ -1,5 +1,28 @@
 package com.nothing.none.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
     
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(crsf -> crsf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated())
+            .formLogin(withDefaults())
+            .build();
+    }
 }
--- src/main/java/com/nothing/none/controller/AuthController.java
+++ src/main/java/com/nothing/none/controller/AuthController.java
@@ -1,5 +1,23 @@
 package com.nothing.none.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
     
+    @PostMapping("/login")
+    public String login() {
+        return "Login";
+    }
+
+    @PostMapping("/register")
+    public String register(){
+        return "Register";
+    }
 }
